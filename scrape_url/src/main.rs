3fbc38267fb4b6040ec3ba345c21b8271bf73305
ice-hydra <icehydra666@gmail.com>
2021-10-16 16:32:17 +0800

    第一个实用的Rust程序

---
+++ scrape_url/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let url = "https://www.rust-lang.org/";
+    let output = "rust.md";
+
+    println!("Fetching url: {}", url);
+    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+
+    println!("Converting html to markdown...");
+    let md = html2md::parse_html(&body);
+
+    std::fs::write(output, md.as_bytes()).unwrap();
+    println!("Converted markdown has heen save in {}.", output);
+}
